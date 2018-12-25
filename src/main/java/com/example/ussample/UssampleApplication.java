aec1fa10168467d86966bc70c19fba6e23de2778
k-maeda5 <k-maeda@atware.co.jp>
2018-12-25 10:57:25 +0900

    initial commit

---
+++ src/main/java/com/example/ussample/UssampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.ussample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UssampleApplication.class, args);
+    }
+
+}
+
